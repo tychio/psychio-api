c073ffa21bcf3e55813c5ab94068786999430f0c
Tychio <code@tychio.net>
2017-11-08 22:49:25 +0800

    optional barriers

--- app/models/leapq_sample_barrier.rb
+++ app/models/leapq_sample_barrier.rb
@@ -1,13 +1,13 @@
 class LeapqSampleBarrier < ApplicationRecord
   def self.save(sampleId, barriers)
-    results = barriers[:results]
+    results = barriers[:results] || {}
     self.create({
       :sample_id => sampleId,
-      :vision => results[:vision],
-      :hearing => results[:hearing],
-      :language => results[:language],
-      :study => results[:study],
-      :explanation => barriers[:explanation]
+      :vision => results[:vision] || 0,
+      :hearing => results[:hearing] || 0,
+      :language => results[:language] || 0,
+      :study => results[:study] || 0,
+      :explanation => barriers[:explanation] || ''
     })
   end
 end
