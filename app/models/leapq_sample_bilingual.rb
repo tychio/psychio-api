5ec6730c1cbd067ee842056b49d05662d59e3355
Tychio <code@tychio.net>
2017-11-16 22:52:47 +0800

    fix snytax

--- app/models/leapq_sample_bilingual.rb
+++ app/models/leapq_sample_bilingual.rb
@@ -1,13 +1,13 @@
 class LeapqSampleBilingual < ApplicationRecord
   def self.save (sampleId, billingual)
     lanuages = billingual[:languages]
-    if (lanuages) {
+    if lanuages
       self.create({
         :sample_id => sampleId,
         :first_language_id => LeapqLanguage.get_id_by_name(lanuages[0]),
         :second_language_id => LeapqLanguage.get_id_by_name(lanuages[1]),
         :period => billingual[:value]
       })
-    }
+    end
   end  
 end
