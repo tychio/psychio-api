ae4a7ddb4673d5fde15b1aa3cee438c619518033
Tychio <code@tychio.net>
2017-11-16 22:41:23 +0800

    fix case when bilingual is empty

--- app/models/leapq_sample_bilingual.rb
+++ app/models/leapq_sample_bilingual.rb
@@ -1,11 +1,13 @@
 class LeapqSampleBilingual < ApplicationRecord
   def self.save (sampleId, billingual)
     lanuages = billingual[:languages]
-    self.create({
-      :sample_id => sampleId,
-      :first_language_id => LeapqLanguage.get_id_by_name(lanuages[0]),
-      :second_language_id => LeapqLanguage.get_id_by_name(lanuages[1]),
-      :period => billingual[:value]
-    })
+    if (lanuages) {
+      self.create({
+        :sample_id => sampleId,
+        :first_language_id => LeapqLanguage.get_id_by_name(lanuages[0]),
+        :second_language_id => LeapqLanguage.get_id_by_name(lanuages[1]),
+        :period => billingual[:value]
+      })
+    }
   end  
 end
