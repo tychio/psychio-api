96ef27419872e9a6ff0b1607d57c44f72ca31e63
Tychio <code@tychio.net>
2017-12-11 00:05:50 +0800

    set header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  before_filter :current_user, :cors_preflight_check
+
+  def cors_preflight_check
+    headers['Access-Control-Allow-Origin'] = 'https://psychio.github.io'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
 end
