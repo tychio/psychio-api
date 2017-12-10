d6ea7d1cc2f82c0a3cae732330363b4498a15655
Tychio <code@tychio.net>
2017-12-11 00:28:24 +0800

    fix header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_filter :current_user, :cors_preflight_check
 
   def cors_preflight_check
-    headers['Access-Control-Allow-Origin'] = 'https://psychio.github.io'
+    headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, GET, OPTIONS'
     headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
