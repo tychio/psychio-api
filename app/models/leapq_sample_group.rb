af94cc83b0558bc786f551d437901a1584d37d42
Tychio <code@tychio.net>
2018-06-14 22:52:59 +0200

    fix group migration for active sample

--- app/models/leapq_sample_group.rb
+++ app/models/leapq_sample_group.rb
@@ -2,7 +2,7 @@ class LeapqSampleGroup < ApplicationRecord
   enum group: [:lang1, :lang2, :balance]
 
   def self.create_by_phone(phone, group)
-    sample = LeapqSample.find_by :phone => phone
+    sample = LeapqSample.find_by({:phone => phone, :is_active => 1})
     if sample.present?
       sample_group = self.find_or_create_by({
         :sample_id => sample.id
